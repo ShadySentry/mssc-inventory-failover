534975371b2fa79c17b115b825334c3e17989ae8
ShadySentry <eurohawk@gmail.com>
2022-08-03 15:16:12 +0300

    added inventory failover

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -15,15 +17,23 @@ import java.util.UUID;
  */
 @Component
 public class InventoryHandler {
-    public Mono<ServerResponse> listInventory(ServerRequest request){
+    public Mono<ServerResponse> listInventory(ServerRequest request) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
-                    .id(UUID.randomUUID())
-                    .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
-                    .quantityOnHand(999)
-                    .createdDate(OffsetDateTime.now())
-                    .lastModifiedDate(OffsetDateTime.now())
-                        .build()), BeerInventoryDto.class);
+//                .body(Mono.just(BeerInventoryDto.builder()
+//                    .id(UUID.randomUUID())
+//                    .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+//                    .quantityOnHand(999)
+//                    .createdDate(OffsetDateTime.now())
+//                    .lastModifiedDate(OffsetDateTime.now())
+//                        .build()), BeerInventoryDto.class);
+                .body(Mono.just(Arrays.asList(
+                        BeerInventoryDto.builder()
+                                .id(UUID.randomUUID())
+                                .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                                .quantityOnHand(999)
+                                .createdDate(OffsetDateTime.now())
+                                .lastModifiedDate(OffsetDateTime.now())
+                                .build())), List.class);
     }
 }
